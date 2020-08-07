99a1bfcd4786dbd46b281e3dd127bfaf130e3b31
Giuseppe Persiano <giuper@gmail.com>
2020-08-07 17:19:10 +0200

    js

---
+++ js/abstract.js
@@ -0,0 +1,42 @@
+status=0;
+
+function toggle_all(){
+	if (status){
+		hide_all();
+		status=0;
+	} else
+	{ 
+		show_all();
+		status=1;
+	}
+}
+
+function hide_all(){
+	for (i=1;i<=26;i++)
+		hide("abstract"+i);
+}
+function hide(DivId) {
+	var ele = document.getElementById(DivId);
+		ele.style.display = "none";
+} 
+
+function show_all(){
+	for (i=1;i<=26;i++)
+		show("abstract"+i);
+}
+
+function show(DivId) {
+	var ele = document.getElementById(DivId);
+		ele.style.display = "block";
+} 
+
+
+
+function toggle(DivId) {
+	var ele = document.getElementById(DivId);
+	if(ele.style.display == "none") 
+		ele.style.display = "block";
+	else 
+   		ele.style.display = "none";
+} 
+
